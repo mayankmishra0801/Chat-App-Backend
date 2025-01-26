d4c056d6e894129b6a6801a9710ef5383d6402c1
mayankmishra0801 <mayank0801mishra@gmail.com>
2025-01-27 02:54:29 +0530

    messageRoutes

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { connectDB } from "./lib/db.js";
 
 
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 // using import and modular somuse .js extension while importing
 
 dotenv.config();
@@ -20,6 +21,7 @@ app.use(cookieParser())
 
 
 app.use("/api/auth",authRoutes);
+app.use("/api/message",messageRoutes)
 
 
 
