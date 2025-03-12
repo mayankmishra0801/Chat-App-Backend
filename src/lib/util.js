0db9cd2fcb675a301a6d4e3f3b3529480833b107
mayankmishra0801 <mayank0801mishra@gmail.com>
2025-03-12 12:27:02 +0530

    fixes

--- src/lib/util.js
+++ src/lib/util.js
@@ -13,7 +13,7 @@ export const generateToken = (userId,res) =>{
         maxAge: 7*24*60*60*1000,
         httpOnly:true,  // prevent xss attack cross-site scripting
         sameSite:"strict", // CSRF attacks cross-site request forgery attacks
-        secure:process.env.NODE_ENV !== "development"
+        secure:process.env.NODE_ENV === "development"
     })
 
 
